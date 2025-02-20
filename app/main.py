c66cebe62171163ac27e1b003e6bb13cfc3ef537
Marcin Djaczuk <marcindjaczuk@gmail.com>
2025-02-20 19:57:06 +0100

    Solution

--- app/main.py
+++ app/main.py
@@ -10,8 +10,8 @@ def go_to_cafe(friends: list, cafe: Cafe) -> None:
     for friend in friends:
         try:
             cafe.visit_cafe(friend)
-        except VaccineError as e:
-            return e
+        except VaccineError:
+            return "All friends should be vaccinated"
         except NotWearingMaskError:
             masks_to_buy += 1
     if masks_to_buy > 0:
